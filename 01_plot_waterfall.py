855a6e8f631e9ab28957d9afdbd19c941502f90d
Hugh Garsden <hughgarsden@yahoo.com>
2018-03-20 04:40:28 -0400

    New script and options

--- 01_plot_waterfall.py
+++ 01_plot_waterfall.py
@@ -6,10 +6,15 @@ Plot data as calibrated waterfall plot.
 
 """
 import os
+import hickle
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import seaborn as sns
 import tables as tb
+import scipy.signal
 from leda_cal.skymodel import *
 from leda_cal.leda_cal import *
+from leda_cal.dpflgr import *
 
 sns.set_style('white')
 sns.set_context("poster",font_scale=.75)
@@ -18,7 +23,7 @@ ovro_location = ('37.2397808', '-118.2816819', 1183.4839)
 ovro = ephem.Observer(); (ovro.lat, ovro.lon, ovro.elev) = ovro_location
 sun = ephem.Sun()
 
-def quicklook(filename, pol, save, dump):
+def quicklook(filename, pol, save, dump, flag, noshow):
     h5 = tb.open_file(filename)
 
     T_ant = apply_calibration(h5)
@@ -41,13 +46,14 @@ def quicklook(filename, pol, save, dump):
         ax = fig.add_subplot(1, 3, ii+1)
         
         T_flagged = T_ant[key]
-        #T_flagged = rfi_flag(T_ant[key], thr_f=0.07, thr_t=0.07, rho=1.5,
-        #         bp_window_f=8, bp_window_t=8, 
-        #         max_frac_f=0.3, max_frac_t=0.3)
+        if flag:
+          T_flagged = rfi_flag(T_flagged, thr_f=0.2, thr_t=0.2, rho=1.5,
+             bp_window_f=16, bp_window_t=16,
+             max_frac_f=0.5, max_frac_t=0.5)
+
         
-        T_ant[key] = T_flagged
-        
-	if dump: np.save(os.path.basename(filename)[:-3]+"_"+key, T_flagged)
+	if dump: hickle.dump(T_flagged, os.path.basename(filename)[:-3]+"_"+key+".hkl")
+
         max_alt = 0
         pad_length = 70
         padding = np.zeros((T_flagged.shape[0], pad_length))
@@ -61,8 +67,9 @@ def quicklook(filename, pol, save, dump):
           padding[i, :] = 1000+((padding[i, 0]+max_alt)/(2*max_alt))*(10000-1000)
           #print d, lst_stamps[i], ovro.sidereal_time(), float(sun.alt)*180/np.pi, 1000+((sun.alt+max_alt)/(2*max_alt))*(10000-1000)
           
-        T_flagged = np.concatenate((T_flagged, padding), axis=1)
+        T_flagged = np.ma.concatenate((T_flagged, padding), axis=1)
         new_x_high = xlims[1]+pad_length*(xlims[1]-xlims[0])/T_flagged.shape[1]
+
         im = plt.imshow(T_flagged, # / np.median(xx, axis=0), 
                    cmap='viridis', aspect='auto',
                    interpolation='nearest',
@@ -91,8 +98,9 @@ def quicklook(filename, pol, save, dump):
     #plt.tight_layout()
     
     if save:
-      plt.savefig(os.path.basename(filename)[:-3]+"_"+pol+".png")
-    else: 
+      if flag: plt.savefig(os.path.basename(filename)[:-3]+"_"+pol+"_"+"flagged.png")
+      else: plt.savefig(os.path.basename(filename)[:-3]+"_"+pol+".png")
+    if not noshow:
       plt.show()
     
     
@@ -106,10 +114,15 @@ if __name__ == "__main__":
     o.set_description(__doc__)
     o.add_option('--pol', dest='pol', default='A',
       help='Polarization A or B. Default: A')
+    o.add_option('--flag', dest='flag', action='store_true', default=False,
+      help='Apply flagging. Default: False')
     o.add_option('--dump', dest='dump', action='store_true', default=False,
-      help='Dump the data to a file, with filename the same as the h5 but npy extension. Default: False')
+      help='Dump the data to a hickle file, with filename the same as the h5 but hkl extension. Default: False')
     o.add_option('--save', dest='save', action='store_true', default=False,
-      help='Save the plot to an image file, with filename the same as the h5 but png extension. Default: False')
+      help="Save the plot to an image file, with filename the same as the h5 but png extension. Default: False.")
+    o.add_option('--noshow', dest='noshow', action='store_true', default=False,
+      help="Don't display the plot on screen. Useful for batch runs. Default: False.")
+
     opts, args = o.parse_args(sys.argv[1:])
     
     if len(args) != 1:
@@ -121,4 +134,4 @@ if __name__ == "__main__":
       o.print_help()
       exit(1)
 
-    quicklook(filename, opts.pol, opts.save, opts.dump)
+    quicklook(filename, opts.pol, opts.save, opts.dump, opts.flag, opts.noshow)
